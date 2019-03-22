3be7549f52a05a95a5324ac3d7e81bf209fdc1f7
Alexander Fayad <alexanderfayad@Alexanders-MacBook-Pro.local>
2019-03-22 20:00:06 +0100

    new update

--- Shops/Shops/settings.py
+++ Shops/Shops/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'oxr#QHz4TRbqMw8G8(Hs{/p=HeR+m9fV'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', True))
 
-ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost', 'Shops.fbmqgxuxev.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
