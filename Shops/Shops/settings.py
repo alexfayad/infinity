1e0ff0a406340d2556a3fb9300720d7c24c17ba1
amol <amol.freelancing@gmail.com>
2019-07-29 15:18:42 +0530

    database port updated

--- Shops/Shops/settings.py
+++ Shops/Shops/settings.py
@@ -96,7 +96,7 @@ if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'inifinity',
+            'NAME': 'infinity',
             'USER': 'infinity',
             'PASSWORD': 'infinity@snakescript',
             'HOST': 'localhost',
