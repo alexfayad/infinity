40edf4e659ccebb3bc7f85ed852d781335a5a3ae
ambhar <amol.bharmoria@gmail.com>
2019-07-30 13:18:30 +0530

    env updated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,22 +2,13 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-/*export const environment = {
+export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8000/api/',
   vigLink: 'https://redirect.viglink.com',
   vigKey: '3982fa502a2fc8ba008bebe81b46771c',
   assets_url : '../assets'
-};*/
-
-export const environment = {
-  production: true,
-  apiUrl: 'http://159.65.220.179:8000/api/',
-  vigLink: 'https://redirect.viglink.com',
-  vigKey: '3982fa502a2fc8ba008bebe81b46771c',
-  assets_url : '../static/ang/assets'
 };
-
 
 /*
  * For easier debugging in development mode, you can import the following file
