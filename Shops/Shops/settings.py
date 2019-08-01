ba815c6e1874a5f1be1a8d5c2b35f604dff74bb6
amol <amol.freelancing@gmail.com>
2019-08-01 11:42:14 +0530

    static root added

--- Shops/Shops/settings.py
+++ Shops/Shops/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY', default='oxr#QHz4TRbqMw8G82uujfdsf213321')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config("DEBUG", default=True)
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
@@ -152,6 +152,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
