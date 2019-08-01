eb415c7001a8d93a7dff27b5bebc1dc523c2b302
amol <amol.freelancing@gmail.com>
2019-08-01 11:55:34 +0530

    debug false

--- Shops/Shops/settings.py
+++ Shops/Shops/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'Shops.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if not DEBUG:
+if  DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
