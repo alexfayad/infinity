e8dc4ea8a21a917f52eba3b3adfb75f3775524a0
amol <amol.freelancing@gmail.com>
2019-08-05 19:05:31 +0530

    removed  csrf middleware

--- Shops/Shops/settings.py
+++ Shops/Shops/settings.py
@@ -62,13 +62,14 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'Shops.urls'
 
